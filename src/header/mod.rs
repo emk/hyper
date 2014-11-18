3a5a11a3ba6b72d72f199724e6b40854511f685a
Sean McArthur <sean.monstar@gmail.com>
2014-11-17 18:43:25 -0800

    add value_string() method to HeaderView

--- src/header/mod.rs
+++ src/header/mod.rs
@@ -278,6 +278,12 @@ impl<'a> HeaderView<'a> {
     pub fn value<H: Header + HeaderFormat>(&self) -> Option<&'a H> {
         get_or_parse::<H>(self.1).map(|item| downcast(item.read()))
     }
+
+    /// Get just the header value as a String.
+    #[inline]
+    pub fn value_string(&self) -> String {
+        (*self.1.read()).to_string()
+    }
 }
 
 impl<'a> fmt::Show for HeaderView<'a> {
@@ -620,6 +626,7 @@ mod tests {
             assert!(header.is::<ContentLength>());
             assert_eq!(header.name(), Header::header_name(None::<ContentLength>));
             assert_eq!(header.value(), Some(ContentLength(11)));
+            assert_eq!(header.value_string(), "11".to_string());
         }
     }
 
